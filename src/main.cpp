388d38ce320e0fc58f14a37e387eb48f47a144d8
Jertrude Fides O. Seguisa <jertrude.fides@gmail.com>
2019-09-18 04:57:52 +0000

    Input mods
    Signed-off-by: Jertrude Fides O. Seguisa <jertrude.fides@gmail.com>

--- src/main.cpp
+++ src/main.cpp
@@ -1,23 +1,44 @@
 #include <iostream>
 #include <cstdint>
-#include <vector>
+#include <array>
+#include <cstdlib>
 #include <algorithm>
+
 
 #include <enigma.hpp>
 
 int main(int argc, char** argv)
 {
     enigma myEnigma;
-    myEnigma.setInitialConfig(25, 25, 10);
+	unsigned long dials[3];
+	
+	if (4 != argc)
+	{
+		std::cout << "Invalid Number of Dials!!!" << std::endl;
+		return EXIT_FAILURE;
+	}
+	
+	for (auto i = 1; i < argc; ++i)
+	{
+		auto dialNum = strtoul(argv[i], NULL, 10);
+		if (dialNum < 1 || dialNum > 26)
+		{
+			std::cout << "Invalid Value of a Dial" << std::endl;
+			return EXIT_FAILURE;
+		}
+			
+		dials[i - 1] = dialNum;
+	}
 
+	myEnigma.setInitialConfig(dials[0], dials[1], dials[2]);
     const std::string data = "supercalifragilisticexpialidocious";
     std::cout << "original: " << data << std::endl;
     auto result = myEnigma.encode(data);
     std::cout << "encrypted: " << result << std::endl;
     
-    myEnigma.setInitialConfig(25, 25, 10);
+    myEnigma.setInitialConfig(dials[0], dials[1], dials[2]);
     result = myEnigma.encode(result);
     std::cout << "recovered: " << result << std::endl;
 
-    return 0;
+    return EXIT_SUCCESS;
 }
